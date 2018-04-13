bdb8e08f87163644f62593d498b56f28925351a3
Plasma <nplusplus538@gmail.com>
2018-04-13 11:55:03 +0900

    プレーンテキスト追加

--- get_userpolicy.sh
+++ get_userpolicy.sh
@@ -4,16 +4,22 @@ url="https://webview.fate-go.jp/webview/userpolicy/index.html"
 date=`date --iso`
 
 curl $url > tmp.html
-diff userpolicy.html tmp.html > diff_$date
-if [ -s diff_$date ]; then
+diff userpolicy.html tmp.html > diff_${date}_html
+
+if [ -s diff_${date}_html ]; then
   echo "差分が見つかりました"
-  cat diff_$date
+  cat diff_${date}_html
+
+  cat tmp.html | sed -e 's/<[^>]*>//g' | sed -e 's/^[ \t]+//g' > tmp.txt
+  diff userpolicy.txt tmp.txt > diff_$date
+
+  rm tmp*
 
   git add userpolicy.html
   git commit -m $date
   git push origin master
 else
   echo "差分は見つかりませんでした"
-  rm diff_$date
+  rm diff_${date}_html
+  rm tmp.html
 fi
-rm tmp.html
